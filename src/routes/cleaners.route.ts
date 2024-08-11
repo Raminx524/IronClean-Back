285c5a617e4feddb385cfec319c1c9f85857627c
RonGamarnik <rgam2002@gmail.com>
2024-08-11 12:55:14 +0300

    12:55

--- src/routes/cleaners.route.ts
+++ src/routes/cleaners.route.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getAllCleaners } from "../controllers/cleaners.controller";
+import { getAllCleaners, getCleanerById, getReservationsByCleanerId, getReviewsByCleanerId, } from "../controllers/cleaners.controller";
+import { postReviwe } from "../controllers/reviwes.controllers";
 
 const cleanersRoutes = Router();
 
 cleanersRoutes.get("/", getAllCleaners);
+cleanersRoutes.get('/:id', getCleanerById)
+cleanersRoutes.get('/:id/reviwes', getReviewsByCleanerId) 
+cleanersRoutes.get('/:id/reservations', getReservationsByCleanerId) 
+cleanersRoutes.post('/:id/review', postReviwe)
 
 export default cleanersRoutes;
