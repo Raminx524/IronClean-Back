46bbba7e9f2e8be747fc5ea35841826d12621e77
RonGamarnik <rgam2002@gmail.com>
2024-08-11 23:17:04 +0300

    23:17

--- src/routes/cleaners.route.ts
+++ src/routes/cleaners.route.ts
@@ -14,6 +14,7 @@ import { verifyToken } from "../middleware/auth.middleware";
 const cleanersRoutes = Router();
 
 cleanersRoutes.get("/", getAllCleaners);
+
 cleanersRoutes.get("/top", get5TopCleaners);
 cleanersRoutes.get("/:id", getCleanerById);
 cleanersRoutes.get("/:id/reviews", getReviewsByCleanerId);
@@ -22,6 +23,5 @@ cleanersRoutes.get("/:id/reservations/reserv", getReservationsByDate);
 cleanersRoutes.get("/:id/days", getDaysById);
 cleanersRoutes.post("/:id/review", postReviwe);
 cleanersRoutes.delete("/:id/review/:revId", verifyToken, deleteReview);
-
 
 export default cleanersRoutes;
