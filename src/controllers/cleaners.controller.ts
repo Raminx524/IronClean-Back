fbe581459860a2d51b42a171959a6bf002b434aa
RonGamarnik <rgam2002@gmail.com>
2024-08-11 12:55:14 +0300

    12:55

--- src/controllers/cleaners.controller.ts
+++ src/controllers/cleaners.controller.ts
@@ -1,11 +1,11 @@
 import { db } from "../app";
 import { Request, Response } from "express";
+import { RowDataPacket, QueryError, FieldPacket } from "mysql2";
 
 export const getAllCleaners = (req: Request, res: Response): void => {
-  const sql =
-    'SELECT * FROM cleaners JOIN users ON users.ID = cleaners.user_id WHERE users.role = "cleaner";';
+  const sql = 'SELECT * FROM cleaners JOIN users ON users.ID = cleaners.user_id WHERE users.role = "cleaner";';
 
-  db.query(sql, (err: Error, results: any[]) => {
+  db.query(sql, (err: QueryError | null, results: RowDataPacket[], fields: FieldPacket[]) => {
     if (err) {
       console.error("Error fetching cleaners:", err);
       res.status(500).send("Server error");
@@ -14,3 +14,47 @@ export const getAllCleaners = (req: Request, res: Response): void => {
     res.status(200).json(results);
   });
 };
+
+export const getCleanerById = (req: Request, res: Response): void => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM cleaners JOIN users ON users.ID = cleaners.user_id WHERE users.role = "cleaner" AND user_id = ?;`;
+
+  db.query(sql, [id], (err: QueryError | null, result: RowDataPacket[], fields: FieldPacket[]) => {
+    if (err) {
+      console.error('Error fetching cleaner by id:', err);
+      res.status(500).send('Server error');
+      return;
+    }
+    if (result.length === 0) {
+      res.status(404).send('Cleaner not found');
+      return;
+    }
+    res.status(200).json(result[0]);
+  });
+};
+
+export const getReviewsByCleanerId = (req: Request, res: Response): void => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM reviews WHERE Posted_ID =?;`;
+  db.query(sql, [id], (err: QueryError | null, results: RowDataPacket[], fields: FieldPacket[]) => {
+    if (err) {
+      console.error('Error fetching reviews by cleaner id:', err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(200).json(results);
+  });
+}
+
+export const getReservationsByCleanerId = (req: Request, res: Response): void => {
+  const id = req.params.id;
+  const sql = `SELECT * FROM reservations WHERE cleaner_id =?;`;
+  db.query(sql, [id], (err: QueryError | null, results: RowDataPacket[], fields: FieldPacket[]) => {
+    if (err) {
+      console.error('Error fetching reservations by cleaner id:', err);
+      res.status(500).send('Server error');
+      return;
+    }
+    res.status(200).json(results);
+  });
+}
---
+++ src/controllers/reviwes.controllers.ts
@@ -0,0 +1,15 @@
+import { db } from "../app";
+import { Request, Response } from "express";
+import { RowDataPacket, QueryError, FieldPacket } from "mysql2";
+export const postReviwe = (req: Request, res: Response) => {
+    const { Poster_ID, Posted_ID, Rating, Text } = req.body;
+    const sql = `INSERT INTO reviews (Poster_ID, Posted_ID, Rating, Text) VALUES (${Poster_ID}, ${Posted_ID}, ${Rating}, "${Text}");`;
+    db.query(sql, (err: QueryError | null, results: RowDataPacket[], fields: FieldPacket[]) => {
+        if (err) {
+            console.error("Error posting review:", err);
+            res.status(500).send("Server error");
+            return;
+        }
+        res.send("Review posted successfully");
+    });
+}
